ddafc9bad2611ace71cae942dd601dd0f972fbea
SnowFall98 <yordymora98@gmail.com>
2021-06-19 16:47:48 -0500

    Avances 2.0 sub menus

--- src/app/pages/products/best-sales-items/best-sales-items.component.ts
+++ src/app/pages/products/best-sales-items/best-sales-items.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Path } from '../../../config';
-import { OwlCarouselConfig, BackgroundImage } from '../../../functions';
-
+import { OwlCarouselConfig, CarouselNavigation } from '../../../functions';
 import { ProductsService } from '../../../services/products.service';
+import { ActivatedRoute } from '@angular/router';
+import { param } from 'jquery';
+
+declare var jQuery:any;
+declare var $:any;
 
 
 @Component({
@@ -14,9 +18,55 @@ export class BestSalesItemsComponent implements OnInit {
 
   path:String = Path.url;
 
-  constructor() { }
+  constructor(private productsService: ProductsService,private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    /*=============================================
+      Caputarmos el parametro URL
+		=============================================*/
+
+    let params = this.activatedRoute.snapshot.params["params"];
+    /*=============================================
+      Filtrar la data de productos con la categoria
+		=============================================*/
+
+    this.productsService.getFilterData("category", params)
+    .subscribe(resp1=>{
+
+      if (Object.keys(resp1).length > 0 ){
+
+        let i;
+      
+        for (i in resp1){
+          this.productsFnc(resp1);
+  
+        }
+      }else {
+        /*=============================================
+        Filtramos Data de subcategorías
+        =============================================*/
+        this.productsService.getFilterData("sub_category", params)
+        .subscribe(resp2=>{
+    
+          let i;
+            
+          for (i in resp2){
+            this.productsFnc(resp2);
+
+          }
+        })
+      }
+
+    })
+    OwlCarouselConfig.fnc();
+    CarouselNavigation.fnc();
+  }
+  /*=============================================
+  Declaramos la función para mostrar las mejores ventas
+  =============================================*/
+  productsFnc(response){
+
   }
 
 }
