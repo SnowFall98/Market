5151dfb4df2fa2194b4a83e397b27f20247052d3
SnowFall98 <yordymora98@gmail.com>
2021-06-21 17:21:03 -0500

    Cambios en el diseño y funciones de valores dinamicos

--- src/app/pages/products/best-sales-items/best-sales-items.component.ts
+++ src/app/pages/products/best-sales-items/best-sales-items.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Path } from '../../../config';
-import { OwlCarouselConfig, CarouselNavigation } from '../../../functions';
+import { OwlCarouselConfig, CarouselNavigation, Rating } from '../../../functions';
 import { ProductsService } from '../../../services/products.service';
 import { ActivatedRoute } from '@angular/router';
-import { param } from 'jquery';
 
 declare var jQuery:any;
 declare var $:any;
@@ -19,6 +18,10 @@ export class BestSalesItemsComponent implements OnInit {
   path:String = Path.url;
   bestSalesItem:Array<any> = [];
   render:Boolean = true;
+  rating:Array<any> = [];
+  reviews:Array<any> = [];
+	price:Array<any> = [];
+	cargando:Boolean = false;
 
   constructor(private productsService: ProductsService,private activatedRoute: ActivatedRoute) { }
 
@@ -38,12 +41,8 @@ export class BestSalesItemsComponent implements OnInit {
 
       if (Object.keys(resp1).length > 0 ){
 
-        let i;
-      
-        for (i in resp1){
           this.productsFnc(resp1);
   
-        }
       }else {
         /*=============================================
         Filtramos Data de subcategorías
@@ -51,12 +50,8 @@ export class BestSalesItemsComponent implements OnInit {
         this.productsService.getFilterData("sub_category", params)
         .subscribe(resp2=>{
     
-          let i;
-            
-          for (i in resp2){
             this.productsFnc(resp2);
 
-          }
         })
       }
 
@@ -97,13 +92,141 @@ export class BestSalesItemsComponent implements OnInit {
     =============================================*/
 
     getSales.forEach((product, index)=>{
-      if (index > 10){
+      if (index < 10){
 
         this.bestSalesItem.push(product);
+        this.rating.push(this.dinamicRating(this.bestSalesItem[index]));
+        this.reviews.push(this.dinamicReviews(this.rating[index]));
+        this.price.push(this.dinamicPrice(this.bestSalesItem[index]));
+
+				this.cargando = false;
 
       }
     })
   }
+
+  /*=============================================
+	Función para el Rating dinamico
+	=============================================*/
+
+  dinamicRating(response){
+
+    let totalReview = 0;
+    let rating = 0;
+    for (let i = 0; i < JSON.parse(response.reviews).length; i++){
+
+      totalReview += Number(JSON.parse(response.reviews)[i]["review"]);
+    }
+
+    rating = Math.round(totalReview/JSON.parse(response.reviews).length);
+
+    return rating;
+
+  }
+
+  /*=============================================
+	Función para el Review dinamico
+	=============================================*/
+
+  dinamicReviews(response){
+
+    /*=============================================
+    Clasificamos la cantidad de estrellas según la calificación
+    =============================================*/    
+
+    let reviews = [];
+
+    for(let r = 0; r < 5; r++){
+
+        if(response < (r+1)){
+
+            reviews[r] = 2
+        
+        }else{
+
+            reviews[r] = 1
+        }    
+    }
+
+    return reviews;
+  }
+
+  /*=============================================
+	Función para los precios dinámicos
+	=============================================*/
+
+  dinamicPrice (response){
+    let type;
+    let value;
+    let offer;
+    let price;
+    let disccount;
+    let arrayPrice = [];
+    let offerDate;
+    let today = new Date();
+
+
+    if(response.offer != ""){
+
+        offerDate = new Date(
+
+            parseInt(JSON.parse(response.offer)[2].split("-")[0]),
+            parseInt(JSON.parse(response.offer)[2].split("-")[1])-1,
+            parseInt(JSON.parse(response.offer)[2].split("-")[2])
+
+        )
+
+        if(today < offerDate){
+
+            type = JSON.parse(response.offer)[0];
+            value = JSON.parse(response.offer)[1];
+
+            if(type == "Disccount"){
+
+                offer = (response.price-(response.price * value/100)).toFixed(2)    
+            }    
+
+            if(type == "Fixed"){
+
+                offer = value;
+                value = Math.round(offer*100/response.price);
+
+            }
+
+            disccount = `<div class="ps-product__badge">-${value}%</div>`;
+
+            price = `<p class="ps-product__price sale">$<span class="end-price">${offer}</span> <del>$${response.price} </del></p>`;
+
+        }else{
+
+            price = `<p class="ps-product__price">$<span class="end-price">${response.price}</span></p>`; 
+        }
+
+    }else{
+
+        price = `<p class="ps-product__price">$<span class="end-price">${response.price}</span></p>`;
+    }
+
+    /*=============================================
+    Definimos si el producto tiene stock
+    =============================================*/    
+
+    if(response.stock == 0){
+
+        disccount = `<div class="ps-product__badge out-stock">Out Of Stock</div>`;
+
+    }
+
+    arrayPrice[0] = price;
+    arrayPrice[1] = disccount;
+
+    return arrayPrice;
+  }
+  
+
+  /*=============================================
+	Función que nos avisa cuando finaliza el renderizado de Angular
+	=============================================*/
 
   callback(){
     if(this.render){
@@ -111,6 +234,7 @@ export class BestSalesItemsComponent implements OnInit {
       
       OwlCarouselConfig.fnc();
       CarouselNavigation.fnc();
+      Rating.fnc();
     }
   }
 
