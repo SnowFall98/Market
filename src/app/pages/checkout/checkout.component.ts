e38571bdd37dfd69be58d8050135a1206b1603c9
SnowFall98 <yordymora98@gmail.com>
2021-09-01 18:00:24 -0500

    Base del sistema de pago PayU - Pendiente hacer funciones por validacion dinamica

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -560,7 +560,47 @@ export class CheckoutComponent implements OnInit {
 
 		}else if (f.value.paymentMethod =="payu"){
 
+			/*=============================================
+			Checkout con Payu
+			=============================================*/
+
+			/*=============================================
+			Formulario web checkout de Payu
+			=============================================*/
+
+			let formPayu = `
+
+			<img src="assets/img/payment-method/payu_logo.png" style="width:100px" />
+
+			<form method="post" action="https://sandbox.checkout.payulatam.com/ppp-web-gateway-payu/">
+				<input name="merchantId"    type="hidden"  value="508029"   >
+				<input name="accountId"     type="hidden"  value="512321" >
+				<input name="description"   type="hidden"  value="Test PAYU"  >
+				<input name="referenceCode" type="hidden"  value="TestPayU" >
+				<input name="amount"        type="hidden"  value="20000"   >
+				<input name="tax"           type="hidden"  value="3193"  >
+				<input name="taxReturnBase" type="hidden"  value="16806" >
+				<input name="currency"      type="hidden"  value="COP" >
+				<input name="signature"     type="hidden"  value="7ee7cf808ce6a39b17481c54f2c57acc"  >
+				<input name="test"          type="hidden"  value="0" >
+				<input name="buyerEmail"    type="hidden"  value="test@test.com" >
+				<input name="responseUrl"    type="hidden"  value="http://www.test.com/response" >
+				<input name="confirmationUrl"    type="hidden"  value="http://www.test.com/confirmation" >
+				<input name="Submit" type="submit" class="ps-btn p-0 px-5" value="Next" >
+			</form>`;
+
+			/*=============================================
+			Sacar el botón de Payu en una alerta suave
+			=============================================*/	
+
+			Sweetalert.fnc("html", formPayu, null);
+
+
 		}else if (f.value.paymentMethod ="mercado-pago"){
+
+
+
+
 
 		}else {
 
