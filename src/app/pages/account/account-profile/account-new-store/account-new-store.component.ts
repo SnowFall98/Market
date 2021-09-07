98d97b161cbc1201b41683bae7f4f910ad33809d
SnowFall98 <yordymora98@gmail.com>
2021-09-07 13:00:09 -0500

    Creación del módulo New Store

---
+++ src/app/pages/account/account-profile/account-new-store/account-new-store.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account-new-store',
+  templateUrl: './account-new-store.component.html',
+  styleUrls: ['./account-new-store.component.css']
+})
+export class AccountNewStoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
