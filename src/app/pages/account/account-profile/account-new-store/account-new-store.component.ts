9bc755dc7f41e96f3a6cd86d0c3da08c2e26d63d
SnowFall98 <yordymora98@gmail.com>
2021-09-07 14:24:21 -0500

    Configuracion para los terminos y condiciones

--- src/app/pages/account/account-profile/account-new-store/account-new-store.component.ts
+++ src/app/pages/account/account-profile/account-new-store/account-new-store.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Path } from '../../../../config';
+import { StoresService } from '../../../../services/stores.service';
+
+declare var jQuery:any;
+declare var $:any;
 
 @Component({
   selector: 'app-account-new-store',
@@ -7,12 +11,42 @@ import { Path } from '../../../../config';
   styleUrls: ['./account-new-store.component.css']
 })
 export class AccountNewStoreComponent implements OnInit {
-
+  
+  @Input() childItem:any;
+  
   path:string = Path.url;
 
-  constructor() { }
+  constructor(private storesService:StoresService) { }
 
   ngOnInit(): void {
+
+    /*=============================================
+    Validamos si el usuario ya tiene una tienda habilitada
+    =============================================*/
+
+    this.storesService.getFilterData("username", this.childItem)
+    .subscribe(resp=>{
+
+      if(Object.keys(resp).length > 0){
+
+        window.open("account/my-store", "_top");
+
+      }
+    })
+  }
+
+  /*=============================================
+  Mover el Scroll hasta donde inicia términos y condiciones
+  =============================================*/
+
+  goTerms(){
+
+    $("html, body").animate({
+
+      scrollTop: $("#tabContent").offset().top-50
+
+    }) 
+
   }
 
 }
