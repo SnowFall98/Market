f1cd5330e72f74ecbee3a09e634fabf81e9dc4bb
SnowFall98 <yordymora98@gmail.com>
2021-09-13 09:20:32 -0500

    Solución warnings

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Api } from '../config';
+import { Api } from '../config.js';
 
 @Injectable({
   providedIn: 'root'
