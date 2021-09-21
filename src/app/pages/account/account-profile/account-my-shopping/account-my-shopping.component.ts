2871bc22cbbb36a3eb51c67d658bbc717ffdc6c0
SnowFall98 <yordymora98@gmail.com>
2021-09-21 18:11:18 -0500

    Component My Shopping funcional - Validar front - Hacer pruebas

--- src/app/pages/account/account-profile/account-my-shopping/account-my-shopping.component.ts
+++ src/app/pages/account/account-profile/account-my-shopping/account-my-shopping.component.ts
@@ -3,7 +3,15 @@ import { Path } from '../../../../config';
 import { Subject  } from 'rxjs';
 import { OrdersService } from '../../../../services/orders.service';
 import { StoresService } from '../../../../services/stores.service';
+import { UsersService } from 'src/app/services/users.service';
+import { DisputesService } from 'src/app/services/disputes.service';
+import { ProductsService } from 'src/app/services/products.service';
+import { DisputesModel } from 'src/app/models/disputes.model';
+import { NgForm } from '@angular/forms';
+import  {Sweetalert, Rating} from '../../../../functions';
 
+declare var jQuery:any;
+declare var $:any;
 
 @Component({
   selector: 'app-account-my-shopping',
@@ -20,12 +28,26 @@ export class AccountMyShoppingComponent implements OnInit, OnDestroy {
   myShopping: any [] = [];
   process: any [] = [];
 	is_vendor:boolean = false;
+  dispute: DisputesModel;
+	id_order:any[] = [];
+	disputes:any[]=[];
+	username:string;
+	picture:string;
+	method:string;
+	idProduct:string;
+	reviews:any[] = [];
+	render:boolean = false;
 
-  constructor(private ordersService: OrdersService, private storesService:StoresService) { }
+  constructor(private ordersService: OrdersService, private storesService:StoresService, private disputesService:DisputesService,
+    private usersService:UsersService, private productsService:ProductsService) { 
+
+      this.dispute = new DisputesModel();
+      
+    }
 
   ngOnInit(): void {
 
-    /*=============================================
+		/*=============================================
     Agregamos opciones a DataTable
     =============================================*/
 
@@ -35,14 +57,14 @@ export class AccountMyShoppingComponent implements OnInit, OnDestroy {
     }
 
     /*=============================================
-		Validamos si el usuario ya tiene una tienda habilitada
-		=============================================*/
+    Validamos si el usuario ya tiene una tienda habilitada
+    =============================================*/
 
-    this.storesService.getFilterData("username", this.childItem)
-    .subscribe(resp=>{
+      this.storesService.getFilterData("username", this.childItem)
+      .subscribe(resp=>{
 
       if(Object.keys(resp).length > 0){
-        
+
         this.is_vendor = true;
 
       }
@@ -63,12 +85,84 @@ export class AccountMyShoppingComponent implements OnInit, OnDestroy {
 
         this.myShopping.push(resp[i]);
         this.process.push(JSON.parse(resp[i]["process"]));
+        this.id_order.push(i);
+
+        /*=============================================
+        Traemos las disputas
+        =============================================*/
+
+        this.id_order.forEach(order=>{
+
+          this.disputesService.getFilterData("order", order)
+          .subscribe(resp=>{
+
+            if(Object.keys(resp).length > 0){	
+
+              let count = 0;	
+
+              for(const i in resp){
+
+                count++;
+
+                this.storesService.getFilterData("store", resp[i].receiver)
+                .subscribe(resp1=>{
+
+                  for(const f in resp1){
+
+                    resp[i].store = resp1[f];
+                  }
+
+
+                })
+
+                this.usersService.getFilterData("username", resp[i].transmitter)
+                .subscribe(resp1=>{
+
+                  for(const f in resp1){
+
+                    resp[i].user = resp1[f];
+                  }
+
+
+                })
+
+                let localDisputes = this.disputes;
+
+                setTimeout(function(){
+
+                  localDisputes.push(resp[i]);
+
+                },count*1000)
+
+              }
+
+            }
+
+          })
+
+        })
+
+        /*=============================================
+        Traemos las reseñas del producto
+        =============================================*/
+
+        this.productsService.getFilterDataMyStore("url", resp[i].url)
+        .subscribe(resp=>{
+
+          for(const i in resp){
+          
+            this.reviews.push(JSON.parse(resp[i].reviews));
+
+          }
+        
+
+        })
         
       }
 
-    /*=============================================
-    Pintar el render en DataTable
-    =============================================*/	
+      /*=============================================
+      Pintar el render en DataTable
+      =============================================*/	
 
       if(load == this.myShopping.length){
 
@@ -76,11 +170,242 @@ export class AccountMyShoppingComponent implements OnInit, OnDestroy {
 
       }
 
+      Rating.fnc();
+
+    })
+	}
+
+	/*=============================================
+	Función nueva disputa
+	=============================================*/
+	newDispute(id_order, store, user){
+		
+		this.dispute.order = id_order;
+		this.dispute.receiver = store;
+		this.dispute.transmitter = user;
+		this.dispute.date_dispute = new Date();
+
+		/*=============================================
+    Abrir la ventana modal
+    =============================================*/  
+    $("#newDispute").modal() 
+
+	}
+
+	/*=============================================
+	Formulario disputas
+	=============================================*/
+
+	onSubmit(f: NgForm){
+
+		/*=============================================
+    Validamos formulario para evitar campos vacíos
+    =============================================*/
+
+		if(f.invalid ){
+
+			Sweetalert.fnc("error", "Invalid Request", null);
+
+			return;
+
+		}
+		
+		/*=============================================
+    Crear una disputa en la BD
+    =============================================*/
+
+    this.disputesService.registerDatabase(this.dispute, localStorage.getItem("idToken"))
+    .subscribe(resp=>{
+
+			if(resp["name"] != ""){
+
+        Sweetalert.fnc("success", "The dispute was created successfully", "account/my-shopping");  
+			}
+
+		}, err =>{
+
+      Sweetalert.fnc("error", err.error.error.message, null)
+
     })
 
-  }
+	}
 
-  /*=============================================
+	/*=============================================
+	Función nueva reseña
+	=============================================*/
+
+	newReview(user, url){
+
+		/*=============================================
+		Almaceno el usuario
+		=============================================*/
+
+		this.username = user;
+
+		/*=============================================
+		Traemos el producto para capturar su ID
+		=============================================*/
+
+		this.usersService.getFilterData("username", user)
+		.subscribe(resp=>{ 
+			
+			for(const i in resp){
+
+				this.picture = resp[i].picture;
+				this.method = resp[i].method;	
+			}
+
+		});
+
+		/*=============================================
+		Traemos el producto para capturar su ID
+		=============================================*/
+
+		this.productsService.getFilterDataMyStore("url", url)
+		.subscribe(resp=>{ 
+			
+			this.idProduct = Object.keys(resp)[0];
+
+		});
+
+		/*=============================================
+    Abrir la ventana modal
+    =============================================*/  
+
+    $("#newReview").modal() 
+
+
+	}
+
+	/*=============================================
+	Enviar nueva reseña
+	=============================================*/
+
+	submitReview(comment, review){
+
+		/*=============================================
+		Validar que la reseña no venga vacía
+		=============================================*/
+
+		if(review.value == "" || comment.value == ""){
+
+			Sweetalert.fnc("error", "Invalid Request", null);
+
+			return;
+
+		}
+
+		/*=============================================
+		Crear el cuerpo de la reseña
+		=============================================*/
+
+		let body = [{	
+
+			"review":review.value,
+			"comment":comment.value,
+			"name":this.username,
+			"image":this.picture,
+			"method":this.method
+
+		}];
+
+		/*=============================================
+		Traer las reseñas del producto
+		=============================================*/
+
+		this.productsService.getUniqueData(this.idProduct)
+		.subscribe(resp=>{
+			
+			let reviews = JSON.parse(resp["reviews"]);
+
+			/*=============================================
+			Eliminar la reseña creada por el usuario anteriormente
+			=============================================*/
+
+			reviews.forEach((review,index)=>{	
+
+				if(review.name == this.username){
+
+					reviews.splice(index, 1);
+
+				}
+
+			})
+
+			/*=============================================
+			Actualizar la información de las reseñas
+			=============================================*/
+
+			reviews.forEach(review=>{	
+
+				body.push(review);				
+
+			})
+
+			/*=============================================
+			Actualizar las reseñas del producto
+			=============================================*/
+
+			let value = {
+
+				"reviews":JSON.stringify(body)
+			}
+
+			this.productsService.patchData(this.idProduct, value)
+			.subscribe(resp=>{
+
+				if(resp["reviews"] != ""){
+
+				 	Sweetalert.fnc("success", "The review was created successfully", "account/my-shopping");  
+				}
+
+			}, err =>{
+
+        Sweetalert.fnc("error", err.error.error.message, null)
+
+      })
+
+		})
+
+	}
+
+	callback(iReview){
+
+		if(!this.render){
+
+			this.render = true;
+
+			setTimeout(function(){
+
+				let reviews = $("[reviews]");
+
+				for(let i = 0; i < reviews.length; i++){
+
+          for(let r = 0; r < 5; r++){
+
+            $(reviews[i]).append(`
+          
+              <option value="2">${r+1}</option>
+
+            `)
+
+            if($(reviews[i]).attr("reviews") == (r+1)){
+
+              $(reviews[i]).children("option").val(1)
+            }
+          }
+
+        }
+
+        Rating.fnc();
+
+			},100*(iReview+1))
+
+		}
+
+	}
+
+	/*=============================================
 	Destruímos el trigger de angular
 	=============================================*/
 
