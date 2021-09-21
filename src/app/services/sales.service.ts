090808bfb1e0609c9fe4b68a69b7231996eeae7f
SnowFall98 <yordymora98@gmail.com>
2021-09-21 10:52:43 -0500

    Obtener y filtrar datos de sales

--- src/app/services/sales.service.ts
+++ src/app/services/sales.service.ts
@@ -26,4 +26,24 @@ export class SalesService {
 		return this.http.post(`${this.api}/sales.json?auth=${idToken}`, body);
 
 	}
+
+	/*=============================================
+	Filtrado de datos
+	=============================================*/
+
+	getFilterData(orderBy:string, equalTo:string){
+
+		return this.http.get(`${this.api}sales.json?orderBy="${orderBy}"&equalTo="${equalTo}"&print=pretty`);
+
+	}
+
+	/*=============================================
+	Actualizar en Firebase Database
+	=============================================*/
+
+	patchDataAuth(id:string, value:object, idToken:string){
+
+		return this.http.patch(`${this.api}sales/${id}.json?auth=${idToken}`,value);
+
+	}
 }
